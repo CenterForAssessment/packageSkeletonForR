dbc324e7747664177e133da563ff1d6d86b9ecc3
Damian Betebenner <dbetebenner@nciea.org>
2022-04-23 03:50:11 -0700

    Updating packageSkeleton

--- R/zzz.R
+++ R/zzz.R
@@ -5,6 +5,6 @@ function(libname, pkgname) {
 `.onAttach` <-
 function(libname, pkgname) {
 	if (interactive()) {
-		packageStartupMessage(magenta$bold('packageSkeletonForR',paste(paste0(unlist(strsplit(as.character(packageVersion("packageSkeletonForR")), "[.]")), c(".", "-", ".", "")), collapse=""),' (6-21-2021). For help: >help("packageSkeletonForR") or visit https://centerforassessment.github.io/packageSkeletonForR'))
+		packageStartupMessage(magenta$bold('packageSkeletonForR',paste(paste0(unlist(strsplit(as.character(packageVersion("packageSkeletonForR")), "[.]")), c(".", "-", ".", "")), collapse=""),' (4-21-2022). For help: >help("packageSkeletonForR") or visit https://centerforassessment.github.io/packageSkeletonForR'))
 	}
 }
